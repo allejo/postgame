a78af85be4336546ce10e3e0f6794d80184bb74e
Vladimir Jimenez <allejo@me.com>
2019-05-14 23:45:46 -0700

    Add controller to make use of replay summary service

--- src/Controller/ReplayController.php
+++ src/Controller/ReplayController.php
@@ -9,18 +9,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Replay;
+use App\Service\ReplaySummaryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReplayController extends AbstractController
 {
     /**
-     * @Route("/replay", name="replay")
+     * @Route("/replays", name="replay")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('replay/index.html.twig', [
             'controller_name' => 'ReplayController',
+        ]);
+    }
+
+    /**
+     * @Route("/replays/{id}/{filename}", name="replay_show")
+     *
+     * @param int                  $id
+     * @param string               $filename
+     * @param ReplaySummaryService $summaryService
+     *
+     * @return Response
+     */
+    public function show(int $id, string $filename, ReplaySummaryService $summaryService): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $replay = $em->getRepository(Replay::class)->findOneBy([
+            'id' => $id,
+            'fileName' => $filename,
+        ]);
+
+        if ($replay === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $summary = $summaryService->getSummary($replay);
+
+        return $this->render('replay/show.html.twig', [
+            'replay' => $replay,
+            'players' => $summary,
         ]);
     }
 }
