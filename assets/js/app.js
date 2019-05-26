a134331e24a5ed34418dc2d8bf9cdee921365cf7
Vladimir Jimenez <allejo@me.com>
2019-05-25 18:52:08 -0700

    Add player Tabs for their individual stats

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,8 @@ require('../css/app.css');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
+require('../vendor/style-guide/src/js/common');
 require('../vendor/style-guide/src/js/table');
+require('../vendor/style-guide/src/js/tablist');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
