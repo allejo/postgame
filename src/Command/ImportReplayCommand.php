4fb0bc63ee87a412b3eacf3b5616c1d4b4126934
Vladimir Jimenez <allejo@me.com>
2019-06-09 14:03:21 -0700

    Add support for importing directories of replays

--- src/Command/ImportReplayCommand.php
+++ src/Command/ImportReplayCommand.php
@@ -12,10 +12,12 @@ namespace App\Command;
 use allejo\bzflag\networking\Packets\PacketInvalidException;
 use App\Service\ReplayImportService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 class ImportReplayCommand extends Command
 {
@@ -45,27 +47,61 @@ class ImportReplayCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dryRun = $input->getOption('dry-run');
-        $replayFile = $input->getArgument('file');
+        $replayFilePath = $input->getArgument('file');
 
         if ($dryRun) {
             $output->writeln('This command is running in "dry mode" meaning nothing will be persisted to the database');
         }
 
-        $isDir = is_dir($replayFile);
+        $isDir = is_dir($replayFilePath);
 
         if (!$isDir) {
-            $output->writeln(sprintf('Reading replay file: %s', $replayFile));
+            $output->writeln(sprintf('Reading replay file: %s', $replayFilePath));
 
             try {
-                $this->replayService->importReplay($replayFile, $dryRun);
+                $this->replayService->importReplay($replayFilePath, $dryRun);
                 $output->writeln(sprintf('Finished.'));
             } catch (PacketInvalidException $e) {
-                $output->writeln(sprintf('An invalid or corrupted replay file was given (%s).', $replayFile));
+                $output->writeln(sprintf('An invalid or corrupted replay file was given (%s).', $replayFilePath));
                 $output->writeln(sprintf('  %s', $e->getMessage()));
             } catch (\InvalidArgumentException $e) {
-                $output->writeln(sprintf('An invalid filepath was given (%s)', $replayFile));
+                $output->writeln(sprintf('An invalid filepath was given (%s)', $replayFilePath));
                 $output->writeln(sprintf('  %s', $e->getMessage()));
             }
+        } else {
+            $output->writeln(sprintf('Reading replay directory: %s', $replayFilePath));
+
+            $replayExtension = $input->getOption('extension');
+
+            $replayFiles = new Finder();
+            $replayFiles
+                ->in($replayFilePath)
+                ->name(sprintf('*.%s', $replayExtension))
+                ->files()
+            ;
+
+            $progressBar = new ProgressBar($output, $replayFiles->count());
+            $progressBar->start();
+
+            foreach ($replayFiles as $replay) {
+                $replayFile = $replay->getRealPath();
+
+                try {
+                    $this->replayService->importReplay($replayFile, $dryRun);
+                } catch (PacketInvalidException $e) {
+                    $output->writeln(sprintf('An invalid or corrupted replay file was given (%s).', $replayFile));
+                    $output->writeln(sprintf('  %s', $e->getMessage()));
+                } catch (\InvalidArgumentException $e) {
+                    $output->writeln(sprintf('An invalid filepath was given (%s)', $replayFile));
+                    $output->writeln(sprintf('  %s', $e->getMessage()));
+                }
+
+                $progressBar->advance();
+            }
+
+            $progressBar->finish();
+
+            $output->writeln('Finished.');
         }
     }
 }
