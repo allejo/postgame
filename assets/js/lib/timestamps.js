8bb6d6cea59c60bd57b075a317303ccfef96c0ad
Vladimir Jimenez <allejo@me.com>
2019-06-23 00:36:01 -0700

    Display timezone abbreviations in Replay timestamps

--- assets/js/lib/timestamps.js
+++ assets/js/lib/timestamps.js
@@ -1,5 +1,14 @@
 import dayjs from 'dayjs';
 
+/**
+ * Get the timezone abbreviation from a date.
+ *
+ * @param {Date} date
+ */
+function timezoneAbbr(date) {
+    return date.toLocaleTimeString('en-us', { timeZoneName: 'short' }).split(' ')[2];
+}
+
 /** @type {NodeListOf<HTMLTimeElement>} */
 const timeElements = document.querySelectorAll('time[data-format]');
 
@@ -8,5 +17,8 @@ for (let i = 0; i < timeElements.length; i++) {
     const datetime = element.getAttribute('datetime');
     const format = element.getAttribute('data-format');
 
-    element.innerText = dayjs(datetime).format(format);
+    const date = new Date(datetime);
+    const tzAbbr = timezoneAbbr(date);
+
+    element.innerText = dayjs(date).format(format) + ' ' + tzAbbr;
 }
